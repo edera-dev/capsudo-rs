0353f7f868968e07e082a9f2be6919ef246c826a
Ariadne Conill <ariadne@ariadne.space>
2026-07-01 11:53:09 -0700

    add CI, release automation, and cargo-make build tooling

---
+++ hack/ci/assemble-release-assets.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+checksum_sha256() {
+  if type sha256sum >/dev/null 2>&1; then
+    sha256sum "${1}"
+  else
+    shasum -a 256 "${1}"
+  fi
+}
+
+asset() {
+  cp "${1}" "${2}"
+  previous="${PWD}"
+  cd "$(dirname "${2}")"
+  base_file_name="$(basename "${2}")"
+  checksum_sha256 "${base_file_name}" >"${base_file_name}.sha256"
+  cd "${previous}"
+}
+
+if [ -z "${CAPSUDO_TAG_NAME}" ]; then
+  echo "CAPSUDO_TAG_NAME env is missing"
+  exit 1
+fi
+
+if [ -z "${CAPSUDO_PLATFORM}" ]; then
+  echo "CAPSUDO_PLATFORM env is missing"
+  exit 1
+fi
+
+if [ -z "${CAPSUDO_RELEASE_DIR}" ]; then
+  echo "CAPSUDO_RELEASE_DIR env is missing"
+  exit 1
+fi
+
+FORMS="capsudo capsudod capsudod-pwauth"
+TAG_NAME="${CAPSUDO_TAG_NAME}"
+PLATFORM="${CAPSUDO_PLATFORM}"
+
+mkdir -p target/assets
+for FORM in ${FORMS}; do
+  for SOURCE_FILE_PATH in ${CAPSUDO_RELEASE_DIR}; do
+    echo "handling $SOURCE_FILE_PATH"
+    SUFFIX=""
+    if echo "${PLATFORM}" | grep "^windows-" >/dev/null; then
+      SUFFIX=".exe"
+    fi
+    # Strip off the binary name down to the directory.
+    directory="${SOURCE_FILE_PATH%/"$FORM""$SUFFIX"}"
+    # Expand wildcard path
+    artifact_path=$(find "${directory}" -name "${FORM}${SUFFIX}" -type f)
+    echo "Found: ${artifact_path}"
+    asset "${artifact_path}" "target/assets/${FORM}_${TAG_NAME}_${PLATFORM}${SUFFIX}"
+  done
+done
---
+++ hack/ci/upload-release-assets.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+retry() {
+  for i in $(seq 1 10); do
+    if "${@}"; then
+      return 0
+    else
+      sleep "${i}"
+    fi
+  done
+  "${@}"
+}
+
+if [ -z "${CAPSUDO_TAG_NAME}" ]; then
+  echo "CAPSUDO_TAG_NAME env is missing"
+  exit 1
+fi
+
+cd target/assets
+
+retry gh release upload "${CAPSUDO_TAG_NAME}" --clobber ./*
